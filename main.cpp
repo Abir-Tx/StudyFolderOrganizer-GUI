54ce5d92c6e340e2e8048cf77a387c0b1fbfac3e
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2021-03-16 01:22:59 +0600

    Add QSS files & Change app stylesheet
    New QSS files added to change the app style quickly

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 #include "sfo.h"
 #include "appinfo.h"
+#include <QFile>
+
 
 int main(int argc, char *argv[]) {
   // This function must be called before creating the QGuiApplication object
@@ -13,6 +15,13 @@ int main(int argc, char *argv[]) {
   app.setWindowIcon(QIcon(":/res/logo/SFO_v2.ico"));
   app.setApplicationDisplayName(applicationDisplayName);
   app.setEffectEnabled(Qt::UI_AnimateMenu, true);
+
+  //open qss file
+  QFile file(":/res/qss-template/Toolery.qss");
+  file.open(QFile::ReadOnly);
+
+  //setup stylesheet
+  app.setStyleSheet(file.readAll());
 
   sfo window;
   window.setWindowTitle(applicationDisplayName);
